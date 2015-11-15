b2ecdb881e2ef67c666c2e7c540cff15eceea806
Yoann BOUTIN <boutinyoann@gmail.com>
2015-11-15 16:28:34 +0100

    changement interface + remaniement changement de page

--- PLIM-GPS/WelcomePage.xaml.cs
+++ PLIM-GPS/WelcomePage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Windows.Devices.Geolocation;
+﻿using System;
+using System.Collections.Generic;
+using Windows.Devices.Geolocation;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // Pour en savoir plus sur le modèle d’élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkID=390556
@@ -13,14 +17,15 @@ namespace PLIM_GPS
     public sealed partial class WelcomePage : Page
     {
 
-        private Geolocator mGeolocator;
+        private Geolocator geolocator;
 
         public bool isAlreadyTracking { get; set; }
 
-        
         public WelcomePage()
         {
             this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            startButton.Content = "Start";
         }
 
         /// <summary>
@@ -35,9 +40,67 @@ namespace PLIM_GPS
 
         // Invoqué lorsque l'on clique sur le bouton Start / Stop
         // Démarre l'enregistrement de la position
-        private void startButton_Click(object sender, RoutedEventArgs e)
+        private async void startButton_Click(object sender, RoutedEventArgs e)
         {
+            if((String)startButton.Content == "Start")
+            {
+                stateText.Text = "In progress...";
+                stateText.Foreground = new SolidColorBrush(Colors.Orange);
+                geolocator = new Geolocator();
+                geolocator.DesiredAccuracyInMeters = 20;
 
+
+
+
+
+                try
+                {
+                    Geoposition geoposition = await geolocator.GetGeopositionAsync(
+                         maximumAge: TimeSpan.FromMinutes(5),
+                         timeout: TimeSpan.FromSeconds(10)
+                        );
+
+
+                    geoText.Text = geoposition.Coordinate.Point.Position.Latitude.ToString() + ", " + geoposition.Coordinate.Point.Position.Longitude.ToString();
+
+                    
+                }
+                //If an error is catch 2 are the main causes: the first is that you forgot to include ID_CAP_LOCATION in your app manifest. 
+                //The second is that the user doesn't turned on the Location Services
+                catch (Exception ex)
+                {
+                    if ((uint)ex.HResult == 0x80004004)
+                    {
+                        //geolocation.Text = "location  is disabled in phone settings.";
+                    }
+                }
+            }
+            
+            changeButtonState();
         }
+
+        private void changeButtonState()
+        {
+            if ((String)startButton.Content == "Start")
+            {
+                startButton.Content = "Stop";
+                stateText.Text = "Running";
+                stateText.Foreground = new SolidColorBrush(Colors.Green);
+            }
+            else if ((String)startButton.Content == "Stop")
+            {
+                startButton.Content = "Start";
+                stateText.Text = "Stopped";
+                stateText.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private void visualizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(VisualisationPage));
+        }
+        
+
+        
     }
 }
